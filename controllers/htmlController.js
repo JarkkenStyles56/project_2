85e978cc5099660011f1740718630fe2c07f60e3
Kristopher Kaae <kskaae@gmail.com>
2020-11-14 13:25:51 -0700

    Handlebars

--- controllers/htmlController.js
+++ controllers/htmlController.js
@@ -5,20 +5,17 @@ const db = require("../models");
 // Requiring our custom middleware for checking if a user is logged in
 const isAuthenticated = require("../config/middleware/isAuthenticated");
 
+//  * Home Page, again
+//  */
+ router.get("/home", function(req, res) {
+   res.render("index", { user: req.user });
+ });
 /**
  * Home Page
  */
 router.get("/", function(req, res) {
   res.render("index", { user: req.user });
 });
-
-/**
- * Home Page, again
- */
-router.get("/home", function(req, res) {
-  res.render("index", { user: req.user });
-});
-
 /**
  * Signup page
  */
@@ -56,8 +53,30 @@ router.get("/forum", isAuthenticated, function(req, res) {
 /**
  * Generic Error Page
  */
+
+
+router.get("/create", function(req, res) {
+  if (req.user) {
+    res.redirect("/");
+  } else {
+    res.render("create", { user: req.user });
+  }
+});
+router.get("/viewid", function(req, res) {
+   if (req.user) {
+      res.redirect("/");
+    } else {
+    res.render("viewid", { user: req.user });
+  }
+});
+router.get("/viewAuth", function(req, res) {
+  if (req.user) {
+    res.redirect("/");
+  } else {
+    res.render("viewAuth", { user: req.user });
+  }
+});
 router.get("*", function(req, res) {
   res.render("errors/404", { user: req.user });
 });
-
 module.exports = router;
