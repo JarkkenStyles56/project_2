ce3dd2f5cb2eed5391c51c68ef2a17316faaada8
Oscar Grajeda <oscargrajeda2@gmail.com>
2020-11-17 19:27:00 -0700

    updated api routes

--- controllers/index.js
+++ controllers/index.js
@@ -4,12 +4,39 @@ const htmlRoutes = require("./htmlController");
 const authRoutes = require("./authController");
 
 // Auth Routes
-router.use("/auth", authRoutes);
+//router.use("/auth", authRoutes);
+router.use("/auth", function (req, res) {
+    authRoutes.all(function (data) {
+        const hbsObject = {
+            authRoutes: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
 
 // API Routes
-router.use("/api", apiRoutes);
+//router.use("/api", apiRoutes);
+router.use("/api", function (req, res) {
+    apiRoutes.all(function (data) {
+        const hbsObject = {
+            apiRoutes: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
 
 // HTML Routes
-router.use("/", htmlRoutes);
+//router.use("/", htmlRoutes);
+router.use("/", function (req, res) {
+    htmlRoutes.all(function (data) {
+        const hbsObject = {
+            htmlRoutes: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
 
 module.exports = router;
