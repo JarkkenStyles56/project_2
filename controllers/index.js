2ef2ab5fc772e3aaaa416ddef19d9503f1dac2bb
Oscar Grajeda <oscargrajeda2@gmail.com>
2020-11-17 19:06:28 -0700

    api routes part one

--- controllers/index.js
+++ controllers/index.js
@@ -10,6 +10,15 @@ router.use("/auth", authRoutes);
 router.use("/api", apiRoutes);
 
 // HTML Routes
-router.use("/", htmlRoutes);
+//router.use("/", htmlRoutes);
+router.use("/", function(req, res) {
+    htmlRoutes.all(function(data) {
+      const hbsObject = {
+        htmlRoutes: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
 
 module.exports = router;
